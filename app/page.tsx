7d0d1558ee714ace2170626f74fb6453ff5ab87d
mikhailpopkov-laptop <m.popkov.02@mail.ru>
2025-11-06 22:08:44 +0300

    Промежуточный коммит

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
       <Banner date = '5/7'
       time = '7 a.m. PT.'
       title = 'Apple Event'/>
+      <div className="b-categories">
+        <div className="categories__wrapper">
+          
+        </div>
+      </div>
     </>
   )
 }
